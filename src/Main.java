4f8b0319716cc53b9e9723bd0a74823f4097108c
“Zhenya” <dyadicheve@mail.ru>
2023-04-09 21:58:40 +0300

    mainChanged

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
 
-       int bmi = service.calculate(1.87, 98);
+        int bmi = service.calculate(1.87, 98);
         System.out.println(bmi);
     }
 }
